316d50ea6e7d32f69aa5b739253450e5e0deaea2
Ahd Ghazal <ahdgh02@gmail.com>
2024-06-23 11:14:02 +0300

    Firebase system building 14

--- config/firebase.php
+++ config/firebase.php
@@ -50,17 +50,9 @@ return [
              *
              */
 
-             'credentials' => [
-                'type' => env('FIREBASE_TYPE', 'service_account'),
-                'project_id' => env('FIREBASE_PROJECT_ID', ''),
-                'private_key_id' => env('FIREBASE_PRIVATE_KEY_ID', ''),
-                'private_key' => str_replace('\\n', "\n", env('FIREBASE_PRIVATE_KEY', '')),
-                'client_email' => env('FIREBASE_CLIENT_EMAIL', ''),
-                'client_id' => env('FIREBASE_CLIENT_ID', ''),
-                'auth_uri' => env('FIREBASE_AUTH_URI', ''),
-                'token_uri' => env('FIREBASE_TOKEN_URI', ''),
-                'auth_provider_x509_cert_url' => env('FIREBASE_AUTH_PROVIDER_X509_CERT_URL', ''),
-                'client_x509_cert_url' => env('FIREBASE_CLIENT_X509_CERT_URL', ''),
+            'credentials' => [
+                'file' => base_path() . '/' . env('FIREBASE_CREDENTIALS'),
+                'auto_discovery' => true,
             ],
             /*
              * ------------------------------------------------------------------------
