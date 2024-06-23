cf75edaed8f172a5da731a0c8f51e5f54bffefef
Ahd Ghazal <ahdgh02@gmail.com>
2024-06-23 11:06:19 +0300

    Firebase system building 12

--- app/Providers/FirebaseServiceProvider.php
+++ app/Providers/FirebaseServiceProvider.php
@@ -14,12 +14,23 @@ class FirebaseServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('firebase.messaging', function ($app) {
-            $credentials = config('firebase.credentials_file');
-            $factory = (new Factory)
-                ->withServiceAccount($credentials);
+        $this->app->singleton(Firebase::class, function ($app) {
+            $credentials = [
+                'type' => config('firebase.credentials.type'),
+                'project_id' => config('firebase.credentials.project_id'),
+                'private_key_id' => config('firebase.credentials.private_key_id'),
+                'private_key' => config('firebase.credentials.private_key'),
+                'client_email' => config('firebase.credentials.client_email'),
+                'client_id' => config('firebase.credentials.client_id'),
+                'auth_uri' => config('firebase.credentials.auth_uri'),
+                'token_uri' => config('firebase.credentials.token_uri'),
+                'auth_provider_x509_cert_url' => config('firebase.credentials.auth_provider_x509_cert_url'),
+                'client_x509_cert_url' => config('firebase.credentials.client_x509_cert_url'),
+            ];
 
-            return $factory->createMessaging();
+            return (new Factory)
+                ->withServiceAccount($credentials)
+                ->create();
         });
     }
 
