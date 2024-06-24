369872beab55a6d97eec72cc93cc9231db25da9d
Ahd Ghazal <ahdgh02@gmail.com>
2024-06-24 20:19:11 +0300

    Reminders setup test3

---
+++ database/migrations/2024_06_24_171142_update_reminders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateRemindersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reminders', function (Blueprint $table) {
+            if (!Schema::hasColumn('reminders', 'garden_plant_entry_id')) {
+                $table->unsignedBigInteger('garden_plant_entry_id')->nullable();
+            }
+            if (!Schema::hasColumn('reminders', 'plant_entry_id')) {
+                $table->unsignedBigInteger('plant_entry_id')->default(0);
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reminders', function (Blueprint $table) {
+            if (Schema::hasColumn('reminders', 'garden_plant_entry_id')) {
+                $table->dropColumn('garden_plant_entry_id');
+            }
+            if (Schema::hasColumn('reminders', 'plant_entry_id')) {
+                $table->dropColumn('plant_entry_id');
+            }
+        });
+    }
+}
